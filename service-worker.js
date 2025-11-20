b5c361c4e828f0959a9f0ecda597ce2d455ca4de
samfiat46-72 <samfiat46@gmail.com>
2025-11-21 03:09:09 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,60 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("the-outfits-cache").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/contact.html",
-        "/privacy.html",
-        "/refunds.html",
-        "/terms.html",
-        "/shipping.html",
-        "/thankyou.html",
-        "/checkout.html",
-      ]);
+const CACHE_NAME = 'the-outfits-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/privacy.html',
+  '/refund.html',
+  '/terms.html',
+  '/shipping.html',
+  '/contact.html',
+  '/logo.png', // Add your logo if exists
+  '/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  console.log('Service Worker: Installed');
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets...');
+      return cache.addAll(ASSETS);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  console.log('Service Worker: Activated');
+
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
     })
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+// Fetch & Cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cacheRes => {
+      return (
+        cacheRes ||
+        fetch(event.request)
+          .then(networkRes => {
+            return caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, networkRes.clone());
+              return networkRes;
+            });
+          })
+          .catch(() => caches.match('/offline.html'))
+      );
     })
   );
 });
